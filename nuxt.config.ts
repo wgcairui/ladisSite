3bd4eda079a107365acf18737da5171e26156ea5
wgcairui <wgcairui@icloud.com>
2020-04-06 08:51:53 +0800

    更新head描述

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,12 +17,7 @@ export default {
     title: defaults.home.key?.title,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description ?? ''
-      }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: defaults.home.ico ?? '/favicon.ico' }]
   },
